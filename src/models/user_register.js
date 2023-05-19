1b72aa6f3b744a3e62129248701d2ab844ef4c0f
Swatantra chaurasia <chaurasiyaswatantra65@gmail.com>
2023-05-19 15:05:43 +0530

    dot env implemented

--- src/models/user_register.js
+++ src/models/user_register.js
@@ -30,7 +30,7 @@ const UserSchema = new Schema({
 
 UserSchema.methods.generateAuthToken = async function() {
     try {
-        const token = jwt.sign({_id:this._id.toString()}, "mynameisswatantrachaurasiyadeveloper");
+        const token = jwt.sign({_id:this._id.toString()}, process.env.SECRET_KEY);
         this.tokens = this.tokens.concat({token:token})
         await this.save();
         return token;
